d8a1e3b4e851ffd5858daa30fe4763ff55de543a
Tomáš Majerčík <tomas.majercik2@gmail.com>
2025-04-29 23:13:11 +0200

    init createTournament page

--- Tournify/app/(tabs)/create.tsx
+++ Tournify/app/(tabs)/create.tsx
@@ -1,23 +1,50 @@
-import { Text, View, StyleSheet } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, KeyboardAvoidingView, Platform, ScrollView, SafeAreaView } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function WelcomeScreen() {
+
+    
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Create Screen</Text>
-        </View>
+        <SafeAreaView style={styles.safeArea}>
+            <KeyboardAvoidingView style={styles.wrapper} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                <ScrollView contentContainerStyle={styles.container}>
+                    <View style={styles.row}>
+                        <MaterialIcons style={styles.icon} name="edit" size={24} color="black" />
+                        <Text style={styles.title}>Create Tournament</Text>
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     );
 }
 
 
 const styles = StyleSheet.create({
-    container: {
+    safeArea: {
         flex: 1,
-        backgroundColor: "#4FC1FF",
-        alignItems: "center",
-        justifyContent: "center",
+        backgroundColor: '#fff',
     },
-    text: {
-        color: "#fff",
-        fontSize: 50,
+    wrapper: {
+        flex: 1,
+        backgroundColor: '#fff',
+      },
+    container: {
+        padding: 24,
     },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    icon: {
+        marginTop: 4,
+        marginRight: 10,
+    },
+    row: {
+        flexDirection: 'row',
+        textAlignVertical: 'center',
+    }
 });
