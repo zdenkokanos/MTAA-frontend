f4241bc6d8e9454bd83900fc7d676771ad25b62d
Zdenko Kanoš <xkanos@stuba.sk>
2025-05-02 22:03:13 +0200

    finally the updated expo works

--- Tournify/app.config.js
+++ Tournify/app.config.js
@@ -5,7 +5,7 @@ export default {
     name: "Tourinify",
     slug: "tournify",
     version: "1.0.0",
-    sdkVersion: "52.0.0",
+    sdkVersion: "53.0.0",
     extra: {
       GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
     },
