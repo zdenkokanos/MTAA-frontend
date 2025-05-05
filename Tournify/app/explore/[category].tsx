a579f423186c98d0b3cb77e3629bfa3e0086f6e6
Tomáš Majerčík <tomas.majercik2@gmail.com>
2025-05-05 16:08:50 +0200

    added location to more places

--- Tournify/app/explore/[category].tsx
+++ Tournify/app/explore/[category].tsx
@@ -3,7 +3,6 @@ import { View, Text, StyleSheet, FlatList, SafeAreaView, Platform, RefreshContro
 import { useEffect, useMemo, useState } from 'react';
 import API_BASE_URL from '@/config/config';
 import useLocation from '@/hooks/useLocation';
-import { getDistance } from 'geolib';
 
 import TournamentView from '@/components/explore/tournamentView'
 import { useTheme } from "@/themes/theme";
@@ -15,10 +14,7 @@ import useOnShakeRefresh from '@/hooks/useOnShakeRefresh';
 export default function CategoryTournamentsScreen() {
     const { latitude, longitude, error, getUserLocation } = useLocation();
     const { category } = useLocalSearchParams<{ category: string }>();
-    const [rawTournaments, setRawTournaments] = useState<any[]>([]);
     const [tournaments, setTournaments] = useState<any[]>([]);
-    const [usingActualGPS, setUsingActualGPS] = useState(false);
-
     const [loading, setLoading] = useState(true);
 
     const categoryParts = category?.split('-');
@@ -36,11 +32,11 @@ export default function CategoryTournamentsScreen() {
             const response = await fetch(`${API_BASE_URL}/tournaments?category_id=${categoryId}&user_id=${userId}`);
             const data = await response.json();
 
-            setRawTournaments(Array.isArray(data) ? data : []);
+            setTournaments(Array.isArray(data) ? data : []);
 
         } catch (error) {
             console.error('Error loading tournaments:', error);
-            setRawTournaments([]); // fallback 
+            setTournaments([]); // fallback 
         } finally {
             setLoading(false);
         }
@@ -54,31 +50,6 @@ export default function CategoryTournamentsScreen() {
         getUserLocation();
     }, []);
 
-    // console.log(rawTournaments); // keep to show it actually does something
-
-    useEffect(() => {
-        if (!rawTournaments.length) return;
-
-        if (latitude !== 0 && longitude !== 0) { // if user allowed gps
-            const updated = rawTournaments.map((tt) => {
-                if (tt.latitude && tt.longitude) {
-                    const distanceInMeters = getDistance(
-                        { latitude, longitude },
-                        { latitude: tt.latitude, longitude: tt.longitude }
-                    );
-                    const distanceInKm = Math.round((distanceInMeters / 1000) * 10) / 10;
-                    return { ...tt, distance: distanceInKm };
-                }
-                return tt; // if doesnt have lat/long, keep as is
-            });
-
-            setTournaments(updated);
-            setUsingActualGPS(true);
-        } else { // GPS unavailable, use data from backend
-            setTournaments(rawTournaments);
-        }
-
-    }, [rawTournaments, latitude, longitude]);
 
     const theme = useTheme();
     const styles = useMemo(() => getStyles(theme), [theme]);
@@ -94,7 +65,6 @@ export default function CategoryTournamentsScreen() {
 
     useOnShakeRefresh(onRefresh);
 
-    // console.log(tournaments); // keep to show it actually does something
 
     return (
         <SafeAreaView
@@ -120,7 +90,7 @@ export default function CategoryTournamentsScreen() {
                 ) : (
                     <>
                         <View style={styles.GPSinfo}>
-                            {usingActualGPS ? (
+                            {latitude != 0 ? (
                                 <Text style={styles.GPSinfoText}>(Tournify is using your current location)</Text>
                             ) : (
                                 <Text style={styles.GPSinfoText}>(Tournify is using your prefered location from profile)</Text>
@@ -138,9 +108,13 @@ export default function CategoryTournamentsScreen() {
                                     <TournamentView
                                         title={item.tournament_name}
                                         date={item.date}
-                                        distanceText={`${item.distance} km from you`}
                                         imageUrl={{ uri: `${API_BASE_URL}/category/images/${item.category_image}` }}
                                         tournamentId={item.id}
+                                        lat={item.latitude}
+                                        lon={item.longitude}
+                                        userLat={latitude}
+                                        userLon={longitude}
+                                        defaultDistance={item.distance}
                                     />
                                 </View>
                             )}
